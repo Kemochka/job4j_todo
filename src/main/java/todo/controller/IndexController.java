41faac5f4696772b1c7a8f2b5f826cf69f41bfb7
kemochka <kemaevak@yandex.ru>
2024-08-02 18:21:31 +0300

    add templates, fix IndexController and index.html

--- src/main/java/todo/controller/IndexController.java
+++ src/main/java/todo/controller/IndexController.java
@@ -1,12 +1,31 @@
 package todo.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import todo.service.task.TaskService;
 
 @Controller
+@AllArgsConstructor
 public class IndexController {
-    @GetMapping("index")
-    public String getIndex() {
+    private final TaskService taskService;
+
+    @GetMapping({"/", "/index"})
+    public String getIndex(Model model) {
+        model.addAttribute("tasks", taskService.findAll());
+        return "index";
+    }
+
+    @GetMapping({"/new"})
+    public String getNew(Model model) {
+        model.addAttribute("tasks", taskService.findByDone(false));
+        return "index";
+    }
+
+    @GetMapping({"/done"})
+    public String getDone(Model model) {
+        model.addAttribute("tasks", taskService.findByDone(true));
         return "index";
     }
 }
--- src/main/java/todo/model/task/Task.java
+++ src/main/java/todo/model/task/Task.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Entity
 @Table(name = "tasks")
