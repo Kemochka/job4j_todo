e85543f10493d8d07eb76efc7008ee43be23641d
kemochka <kemaevak@yandex.ru>
2024-08-02 17:12:11 +0300

    fix Task repository

--- src/main/java/todo/repository/task/HbmTaskRepository.java
+++ src/main/java/todo/repository/task/HbmTaskRepository.java
@@ -5,9 +5,11 @@ import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import todo.model.task.Task;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -20,20 +22,24 @@ public class HbmTaskRepository implements TaskRepository {
 
     @Override
     public Optional<Task> save(Task task) {
-        try (Session session = sf.openSession()) {
+        Session session = sf.openSession();
+        try {
             session.beginTransaction();
             session.save(task);
             session.getTransaction().commit();
             return Optional.of(task);
         } catch (HibernateException e) {
             LOGGER.error("Exception during save task", e);
+        } finally {
+            session.close();
         }
         return Optional.empty();
     }
 
     @Override
     public boolean update(Task task) {
-        try (Session session = sf.openSession()) {
+        Session session = sf.openSession();
+        try {
             session.beginTransaction();
             session.createQuery("update Task set description = :fDescription, created = :fCreated, done = :fDone where id = :fId")
                     .setParameter("fDescription", task.getDescription())
@@ -41,32 +47,37 @@ public class HbmTaskRepository implements TaskRepository {
                     .setParameter("fDone", task.isDone())
                     .setParameter("fId", task.getId())
                     .executeUpdate();
-
             session.getTransaction().commit();
             return true;
         } catch (HibernateException e) {
             LOGGER.error("Exception during update task", e);
+        } finally {
+            session.close();
         }
         return false;
     }
 
     @Override
     public boolean delete(Task task) {
-        try (Session session = sf.openSession()) {
+        Session session = sf.openSession();
+        try {
             session.beginTransaction();
             session.delete(task);
             session.getTransaction().commit();
             return true;
         } catch (HibernateException e) {
             LOGGER.error("Exception during delete task");
+        } finally {
+            session.close();
         }
         return false;
     }
 
     @Override
     public boolean setDone(int id) {
+        Session session = sf.openSession();
         boolean result = false;
-        try (Session session = sf.openSession()) {
+        try {
             session.beginTransaction();
             session.createQuery("update Task set done = true where id = :fId")
                     .setParameter("fId", id)
@@ -75,41 +86,59 @@ public class HbmTaskRepository implements TaskRepository {
             result = true;
         } catch (HibernateException e) {
             LOGGER.error("Exception during setDone task");
+        } finally {
+            session.close();
         }
         return result;
     }
 
     @Override
     public Optional<Task> getById(int id) {
-        try (Session session = sf.openSession()) {
+        Session session = sf.openSession();
+        try {
             session.beginTransaction();
             Task task = session.get(Task.class, id);
             session.getTransaction().commit();
             return Optional.of(task);
         } catch (HibernateException e) {
             LOGGER.error("Exception during get task by id");
+        } finally {
+            session.close();
         }
         return Optional.empty();
     }
 
     @Override
     public List<Task> findByDone(boolean isDone) {
-        try (Session session = sf.openSession()) {
+        Session session = sf.openSession();
+        List<Task> tasks = new ArrayList<>();
+        try {
             session.beginTransaction();
             var query = session.createQuery("from Task as t where t.done = :fDone", Task.class)
                     .setParameter("fDone", true);
-            List<Task> tasks = query.getResultList();
+            tasks = query.getResultList();
             session.getTransaction().commit();
-            return tasks;
+        } catch (HibernateException e) {
+            LOGGER.error("Exception during find tasks by done");
+        } finally {
+            session.close();
         }
+        return tasks;
     }
 
     @Override
     public Collection<Task> findAll() {
         Session session = sf.openSession();
-        session.beginTransaction();
-        List<Task> tasks = session.createQuery("from Task", Task.class).list();
-        session.getTransaction().commit();
+        List<Task> tasks = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            tasks = session.createQuery("from Task", Task.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            LOGGER.error("Exception during find all tasks");
+        } finally {
+            session.close();
+        }
         return tasks;
     }
 }
