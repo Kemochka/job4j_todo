f093f36709be8ac1691760d644332af417c6f4cf
kemochka <kemaevak@yandex.ru>
2024-08-04 18:30:13 +0300

    add model User

---
+++ src/main/java/todo/model/user/User.java
@@ -0,0 +1,26 @@
+package todo.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String login;
+    @Column
+    private String password;
+}
