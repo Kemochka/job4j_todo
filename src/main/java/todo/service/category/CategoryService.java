eb0d437e8927d6e9f2bc97f42a516623aee8940d
kemochka <kemaevak@yandex.ru>
2024-08-09 16:19:02 +0300

    add CategoryService

---
+++ src/main/java/todo/service/category/CategoryService.java
@@ -0,0 +1,15 @@
+package todo.service.category;
+
+import todo.model.category.Category;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+
+public interface CategoryService {
+    Optional<Category> save(Category category);
+    Optional<Category> findById(int id);
+    boolean deleteById(int id);
+    Collection<Category> findAll();
+    Set<Category> findCategoriesById(Set<Integer> id);
+}
