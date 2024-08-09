8c85ca20a45e5d131b7dff3151d7f7fc7427d74c
kemochka <kemaevak@yandex.ru>
2024-08-09 15:50:45 +0300

    fix model Task and add model Category

---
+++ src/main/java/todo/model/category/Category.java
@@ -0,0 +1,20 @@
+package todo.model.category;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+@Setter
+@Getter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+}
